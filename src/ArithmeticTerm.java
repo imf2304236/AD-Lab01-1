cd7a7ba496776204a9e3ac24fd70c6747cb3df66
Ian F <ian.fennie@gmail.com>
2017-10-02 15:43:51 +0200

    add init ArithmeticTerm class

---
+++ src/ArithmeticTerm.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Ian Fennie on 02.10.17.
+ */
+public class ArithmeticTerm {
+    private String term;
+
+    public ArithmeticTerm(String input) {
+        term = input;
+    }
+
+    public String toString() {
+
+    }
+
+    public void reverse() {
+
+    }
+
+    public double evaluate() {
+
+    }
+
+    public String getTerm() {
+        return term;
+    }
+}
