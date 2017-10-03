a8b82a4489f1aaa08a8e19705360e225e07d5a81
Ian F <ian.fennie@gmail.com>
2017-10-03 18:37:50 +0200

    * Write init ArithmeticTerm.evaluate() * Add javadoc comments for ArithmeticTerm

--- src/ArithmeticTerm.java
+++ src/ArithmeticTerm.java
@@ -10,14 +10,25 @@ import java.util.StringTokenizer;
 public class ArithmeticTerm {
     private String expression;
 
+    /**
+     * Constructor method
+     * @param input String to be stored as the expression field
+     */
     public ArithmeticTerm(String input) {
         expression = input;
     }
 
+    /**
+     * Getter method for the expression field
+     * @return expression String stored in class
+     */
     public String toString() {
         return expression;
     }
 
+    /**
+     * Method to reverse the order of tokens in the expression field
+     */
     public void reverse() {
 
         StringTokenizer tokenizer = new StringTokenizer(expression);
@@ -31,7 +42,6 @@ public class ArithmeticTerm {
         while (!(stringStack.empty())) {
             stringBuilder.append(stringStack.pop());
 
-
             if (!(stringStack.empty())) {
                 stringBuilder.append(" ");
             }
@@ -40,8 +50,50 @@ public class ArithmeticTerm {
         expression = stringBuilder.toString();
     }
 
+    /**
+     * Method to evaluate the Postfix expression field
+     * @return double value which the expression field evaluates to
+     * @throws NumberFormatException if a
+     */
+    public double evaluate() throws NumberFormatException {
+        double value;
+        StringTokenizer tokenizer = new StringTokenizer(expression);
+        Stack<Double> doubleStack = new Stack<>();
 
-    /*public double evaluate() {
+        while (tokenizer.hasMoreTokens()) {
+            String currentToken = tokenizer.nextToken();
+            double d;
 
-    }*/
+            try {
+                d = Double.parseDouble(currentToken);
+                doubleStack.push(d);
+            } catch (NumberFormatException e) {
+                switch (currentToken) {
+                    case "+":
+                        doubleStack.push(doubleStack.pop() + doubleStack.pop());
+                        break;
+                    case "-":
+                        doubleStack.push(doubleStack.pop() - doubleStack.pop());
+                        break;
+                    case "*":
+                        doubleStack.push(doubleStack.pop() * doubleStack.pop());
+                        break;
+                    case "/":
+                        doubleStack.push(doubleStack.pop() / doubleStack.pop());
+                        break;
+                    case "%":
+                        doubleStack.push(doubleStack.pop() % doubleStack.pop());
+                        break;
+                    default:
+                        throw new IllegalArgumentException(
+                                "Only numerical or operator characters can be evaluated."
+                        );
+                }
+            }
+        }
+
+        value = doubleStack.pop();
+
+        return value;
+    }
 }
