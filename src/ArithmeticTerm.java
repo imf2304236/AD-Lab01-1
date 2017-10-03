7f8b029b0c5ad99a4c7cb12db240136787032bea
Ian F <ian.fennie@gmail.com>
2017-10-03 16:26:55 +0200

    * Rename ArithmeticTerm.term ArithmeticTerm.expression * Write init ArithmeticTerm.reverse()

--- src/ArithmeticTerm.java
+++ src/ArithmeticTerm.java
@@ -1,26 +1,47 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 /**
+ * Labs
  * Created by Ian Fennie on 02.10.17.
  */
 public class ArithmeticTerm {
-    private String term;
+    private String expression;
 
     public ArithmeticTerm(String input) {
-        term = input;
+        expression = input;
     }
 
     public String toString() {
-
+        return expression;
     }
 
     public void reverse() {
 
+        StringTokenizer tokenizer = new StringTokenizer(expression);
+        Stack<String> stringStack = new Stack<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (tokenizer.hasMoreTokens()) {
+            stringStack.push(tokenizer.nextToken());
+        }
+
+        while (!(stringStack.empty())) {
+            stringBuilder.append(stringStack.pop());
+
+
+            if (!(stringStack.empty())) {
+                stringBuilder.append(" ");
+            }
+        }
+
+        expression = stringBuilder.toString();
     }
 
-    public double evaluate() {
 
-    }
+    /*public double evaluate() {
 
-    public String getTerm() {
-        return term;
-    }
+    }*/
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
+        ArithmeticTerm term = new ArithmeticTerm("1 2 3 4 5");
+        term.reverse();
     }
 }
