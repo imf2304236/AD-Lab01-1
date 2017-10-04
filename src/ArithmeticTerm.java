11dd09ffbc1becc0b0b7dde8783b31bb3f24cfff
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-04 12:36:10 +0200

    Encapsulate statements from Main class in ArithmeticTerm.main()

--- src/ArithmeticTerm.java
+++ src/ArithmeticTerm.java
@@ -96,4 +96,17 @@ public class ArithmeticTerm {
 
         return value;
     }
+
+    /**
+     * Test method
+     */
+    public static void main(String[] args) {
+        double value;
+        ArithmeticTerm term = new ArithmeticTerm("1 2 3 4 5");
+        term.reverse();
+        ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 + 4 6 * * 7 + *");
+        value = term2.evaluate();
+        ArithmeticTerm term3 = new ArithmeticTerm("5.1 9 8.88 ? 4 6 * * 7 + *");
+        value = term3.evaluate();
+    }
 }
