cfae9c7d00ece5a03620d4ed268b9201d1fc6fe7
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-08 20:06:53 +0200

    Improve & refactor JUnit testing

--- src/ArithmeticTerm.java
+++ src/ArithmeticTerm.java
@@ -1,6 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import java.util.Stack;
 import java.util.StringTokenizer;
-
 /**
  * ADL Lab 01
  * Ian Fennie: 2304236
@@ -33,6 +35,8 @@ public class ArithmeticTerm {
             } catch (NumberFormatException e) {
                 if (currentToken.matches("[-+*/%]")) {
                     binaryOps++;
+                } else if (currentToken.matches("[()]")) {
+                    continue;
                 } else {
                     throw new IllegalArgumentException("Only numerical or operator characters can be evaluated.");
                 }
@@ -169,62 +173,113 @@ public class ArithmeticTerm {
      * Main method to construct test cases and call test() on each
      */
     public static void main(String args[]) {
-        // Test case constructors
-        ArithmeticTerm term1 = new ArithmeticTerm("5.1 9 /");
-        ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 % /");
-        ArithmeticTerm term3 = new ArithmeticTerm("5.1 9 8.88 * 4 % /");
-        ArithmeticTerm term4 = new ArithmeticTerm("5.1 9 8.88 + 4 6 * % /");
-        ArithmeticTerm term5 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 % /");
+        ArithmeticTerm.test();
 
-        // Test method call on each test case
-        term1.test();
-        term2.test();
-        term3.test();
-        term4.test();
-        term5.test();
-
-        // Illegal character test case
-        ArithmeticTerm term6 = new ArithmeticTerm("5.1 9 8.88 ? 4 6 * * 7 + *");
-
-        // Insufficient operands test case
-        ArithmeticTerm term7 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * % /");
-
-        // Insufficient operators test case
-        ArithmeticTerm term8 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 %");
-
+        ArithmeticTerm.testIllegalCharacter();
+        ArithmeticTerm.testIllegalNumOfOps();
     }
 
     /**
      * Second test method for toString(), convert() & evaluate()
      */
     public static void main2(String args[]) {
-        // Test case constructor
-        ArithmeticTerm term1 = new ArithmeticTerm("( ( 4.3 * 1e-1 ) - .4 )");
-
-        // toString() test
-        System.out.println("term.toString()  : " + term1.toString());
-
-        // convert() test
-        String conversion = term1.convert();
-        System.out.printf("term.convert()   : %s\n", conversion);
-
-        // evaluate() test
-        ArithmeticTerm term2 = new ArithmeticTerm(conversion);
-        System.out.printf("term.evaluate()  : %f\n", term2.evaluate());
+        ArithmeticTerm.testConvert();
     }
 
-    /**
-     * Test method for toString(), evaluate() & reverse()
-     */
-    public void test() {
-        // toString() test
-        System.out.println("term.toString()  : " + this.toString());
+    @Test
+    public static void test() {
+        ArithmeticTerm.testToString();
+        ArithmeticTerm.testEvaluate();
+        ArithmeticTerm.testReverse();
+    }
 
-        // evaluate() test
-        System.out.printf("term.evaluate()  : %f\n", this.evaluate());
+    @Test
+    public static void testToString() {
+        ArithmeticTerm term1 = new ArithmeticTerm("5.1 9 /");
+        assertEquals("5.1 9 /", term1.toString());
 
-        // reverse() test
-        this.reverse();
-        System.out.println("term.reverse()   : " + this.toString());
+        ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 % /");
+        assertEquals("5.1 9 8.88 % /", term2.toString());
+
+        ArithmeticTerm term3 = new ArithmeticTerm("5.1 9 8.88 * 4 % /");
+        assertEquals("5.1 9 8.88 * 4 % /", term3.toString());
+
+        ArithmeticTerm term4 = new ArithmeticTerm("5.1 9 8.88 + 4 6 * % /");
+        assertEquals("5.1 9 8.88 + 4 6 * % /", term4.toString());
+
+        ArithmeticTerm term5 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 % /");
+        assertEquals("5.1 9 8.88 - 4 6 + * 7 % /", term5.toString());
+    }
+
+    @Test
+    public static void testEvaluate() {
+        ArithmeticTerm term1 = new ArithmeticTerm("5.1 9 /");
+        assertEquals(5.1/9, term1.evaluate(), 0.000001);
+
+        ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 % /");
+        assertEquals(5.1 / (9 % 8.88), term2.evaluate(), 0.000001);
+
+        ArithmeticTerm term3 = new ArithmeticTerm("5.1 9 8.88 * 4 % /");
+        assertEquals(5.1 / ((9 * 8.88) % 4), term3.evaluate(), 0.000001);
+
+        ArithmeticTerm term4 = new ArithmeticTerm("5.1 9 8.88 + 4 6 * % /");
+        assertEquals(5.1 / ((9 + 8.88) % (4 * 6)), term4.evaluate(), 0.000001);
+
+        ArithmeticTerm term5 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 % /");
+        assertEquals(5.1 / ((9 - 8.88) * (4 + 6) % 7), term5.evaluate(), 0.000001);
+    }
+
+    @Test
+    public static void testReverse() {
+        ArithmeticTerm term1 = new ArithmeticTerm("5.1 9 /");
+        term1.reverse();
+        assertEquals("/ 9 5.1", term1.toString());
+
+        ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 % /");
+        term2.reverse();
+        assertEquals("/ % 8.88 9 5.1", term2.toString());
+
+        ArithmeticTerm term3 = new ArithmeticTerm("5.1 9 8.88 * 4 % /");
+        term3.reverse();
+        assertEquals("/ % 4 * 8.88 9 5.1", term3.toString());
+
+        ArithmeticTerm term4 = new ArithmeticTerm("5.1 9 8.88 + 4 6 * % /");
+        term4.reverse();
+        assertEquals("/ % * 6 4 + 8.88 9 5.1", term4.toString());
+
+        ArithmeticTerm term5 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 % /");
+        term5.reverse();
+        assertEquals("/ % 7 * + 6 4 - 8.88 9 5.1", term5.toString());
+    }
+
+    @Test
+    public static void testConvert() {
+        ArithmeticTerm term1 = new ArithmeticTerm("( ( 4.3 * 1e-1 ) - .4 )");
+        ArithmeticTerm term2 = new ArithmeticTerm(term1.convert());
+        assertEquals("4.3 1e-1 * .4 - ", term2.toString());
+        assertEquals(4.3 * 1e-1 - 0.4, term2.evaluate(), 0.0001);
+    }
+
+    @Test
+    public static void testIllegalCharacter() {
+        try {
+            ArithmeticTerm term = new ArithmeticTerm("5.1 9 8.88 ? 4 6 * * 7 + *");
+            fail("No IllegalArgumentException thrown for illegal character in expression.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Only numerical or operator characters can be evaluated.", e.getMessage());
+        }
+    }
+
+    @Test
+    public static void testIllegalNumOfOps() {
+        try {
+            ArithmeticTerm term1 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * % /");
+            ArithmeticTerm term2 = new ArithmeticTerm("5.1 9 8.88 - 4 6 + * 7 % /");
+            ArithmeticTerm term3 = new ArithmeticTerm("+ * % /");
+            ArithmeticTerm term4 = new ArithmeticTerm("5.1 9 8.88");
+            fail("No IllegalArgumentException thrown for inproportional numbers of operators and operands");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Number of operands and operators does not match.", e.getMessage());
+        }
     }
 }
